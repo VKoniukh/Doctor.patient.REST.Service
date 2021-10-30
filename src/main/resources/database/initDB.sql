478af7fad5ae0b8913f5fabeb7cf9ef0bab9d131
Володимир <konuhvladimir1@gmail.com>
2021-10-30 22:13:02 +0300

    fix sql script

--- src/main/resources/database/initDB.sql
+++ src/main/resources/database/initDB.sql
@@ -1,34 +1,25 @@
 CREATE TABLE IF NOT EXISTS doctors (
     id         BIGINT AUTO_INCREMENT PRIMARY KEY,
-    firstName       VARCHAR(50)   NOT NULL,
-    lastName        VARCHAR(100)  NOT NULL,
+    first_name       VARCHAR(50)   NOT NULL,
+    last_name        VARCHAR(100)  NOT NULL,
     patronymic      VARCHAR(1000),
     position        VARCHAR(100)  NOT NULL,
-    birthDate       DATE          NOT NULL,
-    phoneNumber     BIGINT        NOT NULL
+    birth_date       DATE          NOT NULL,
+    phone_number     BIGINT        NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS patients (
     id         BIGINT AUTO_INCREMENT PRIMARY KEY,
-    FOREIGN KEY (doctorId)  REFERENCES doctors (Id),
-    doctorId        BIGINT,
-    firstName       VARCHAR(50)     NOT NULL,
-    lastName        VARCHAR(100)    NOT NULL,
+    FOREIGN KEY (doctor_id)  REFERENCES doctors (Id),
+    doctor_id        BIGINT,
+    first_name       VARCHAR(50)     NOT NULL,
+    last_name        VARCHAR(100)    NOT NULL,
     patronymic      VARCHAR(1000),
-    birthDate       DATE            NOT NULL,
-    phoneNumber     BIGINT          NOT NULL
+    birth_date       DATE            NOT NULL,
+    phone_number     BIGINT          NOT NULL
     );
 
-ALTER TABLE doctors RENAME COLUMN firstName TO first_name;
-ALTER TABLE doctors RENAME COLUMN lastName TO last_name;
-ALTER TABLE doctors RENAME COLUMN phoneNumber TO phone_number;
-ALTER TABLE doctors RENAME COLUMN birthDate TO birth_date;
 
-ALTER TABLE patients RENAME COLUMN doctorId TO doctor_id;
-ALTER TABLE patients RENAME COLUMN firstName TO first_name;
-ALTER TABLE patients RENAME COLUMN lastName TO last_name;
-ALTER TABLE patients RENAME COLUMN birthDate TO birth_date;
-ALTER TABLE patients RENAME COLUMN phoneNumber TO phone_number;
 
 
 
